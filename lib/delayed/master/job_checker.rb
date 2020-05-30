687e3f90898c591bf53f7f886c1a80fccbbe7a2f
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2020-05-30 17:48:56 +0900

    Fix database connection after fork

--- lib/delayed/master/job_checker.rb
+++ lib/delayed/master/job_checker.rb
@@ -52,8 +52,9 @@ module Delayed
         prc = @config.after_fork
         @config.after_fork do |master, worker|
           prc.call(master, worker)
-          if worker.database && ActiveRecord::Base.connection_pool.spec.name != worker.database.to_s
+          if worker.database
             ActiveRecord::Base.establish_connection(worker.database)
+            master.logger.info "connected @#{worker.database}"
           end
         end
       end
